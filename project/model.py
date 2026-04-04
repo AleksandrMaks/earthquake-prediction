6f38d66e78f45af5c58a3688a57331f6b303cd38
AleksandrMaks <you@example.com>
2026-04-04 12:13:57 +0300

    change model to RNN

--- project/model.py
+++ project/model.py
@@ -1,94 +1,33 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import pytorch_lightning as pl
-import seaborn as sns
-import torch
-import torchmetrics
-import wandb
-from sklearn.metrics import confusion_matrix
-from transformers import AutoModelForSequenceClassification
-import logging
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import SimpleRNN
+from keras.layers import Dropout
 
-class ColaModel(pl.LightningModule):
-    def __init__(self, model_name="google/bert_uncased_L-2_H-128_A-2", lr=3e-5):
-        super(ColaModel, self).__init__()
-        self.save_hyperparameters()
+# Initialising the RNN
+regressor = Sequential()
 
-        self.bert = AutoModelForSequenceClassification.from_pretrained(
-            model_name, num_labels=2
-        )
-        self.num_classes = 2
-        self.train_accuracy_metric = torchmetrics.Accuracy()
-        self.val_accuracy_metric = torchmetrics.Accuracy()
-        self.f1_metric = torchmetrics.F1(num_classes=self.num_classes)
-        self.precision_macro_metric = torchmetrics.Precision(
-            average="macro", num_classes=self.num_classes
-        )
-        self.recall_macro_metric = torchmetrics.Recall(
-            average="macro", num_classes=self.num_classes
-        )
-        self.precision_micro_metric = torchmetrics.Precision(average="micro")
-        self.recall_micro_metric = torchmetrics.Recall(average="micro")
+# Adding the first RNN layer and some Dropout regularisation
+regressor.add(SimpleRNN(units = 50,activation='tanh', return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
 
-    def forward(self, input_ids, attention_mask, labels=None):
-        outputs = self.bert(
-            input_ids=input_ids, attention_mask=attention_mask, labels=labels
-        )
-        return outputs
+# Adding a second RNN layer and some Dropout regularisation
+regressor.add(SimpleRNN(units = 50,activation='tanh', return_sequences = True))
+regressor.add(Dropout(0.2))
 
-    def training_step(self, batch, batch_idx):
-        outputs = self.forward(
-            batch["input_ids"], batch["attention_mask"], labels=batch["label"]
-        )
-        # loss = F.cross_entropy(logits, batch["label"])
-        preds = torch.argmax(outputs.logits, 1)
-        train_acc = self.train_accuracy_metric(preds, batch["label"])
-        self.log("train/loss", outputs.loss, prog_bar=True, on_epoch=True)
-        self.log("train/acc", train_acc, prog_bar=True, on_epoch=True)
-        return outputs.loss
+# Adding a third RNN layer and some Dropout regularisation
+regressor.add(SimpleRNN(units = 50,activation='tanh', return_sequences = True))
+regressor.add(Dropout(0.2))
 
-    def validation_step(self, batch, batch_idx):
-        labels = batch["label"]
-        outputs = self.forward(
-            batch["input_ids"], batch["attention_mask"], labels=batch["label"]
-        )
-        preds = torch.argmax(outputs.logits, 1)
+# Adding a fourth RNN layer and some Dropout regularisation
+regressor.add(SimpleRNN(units = 50))
+regressor.add(Dropout(0.2))
 
-        # Metrics
-        valid_acc = self.val_accuracy_metric(preds, labels)
-        precision_macro = self.precision_macro_metric(preds, labels)
-        recall_macro = self.recall_macro_metric(preds, labels)
-        precision_micro = self.precision_micro_metric(preds, labels)
-        recall_micro = self.recall_micro_metric(preds, labels)
-        f1 = self.f1_metric(preds, labels)
+# Adding the output layer
+regressor.add(Dense(units = 1))
 
-        # Logging metrics
-        self.log("valid/loss", outputs.loss, prog_bar=True, on_step=True)
-        self.log("valid/acc", valid_acc, prog_bar=True, on_epoch=True)
-        self.log("valid/precision_macro", precision_macro, prog_bar=True, on_epoch=True)
-        self.log("valid/recall_macro", recall_macro, prog_bar=True, on_epoch=True)
-        self.log("valid/precision_micro", precision_micro, prog_bar=True, on_epoch=True)
-        self.log("valid/recall_micro", recall_micro, prog_bar=True, on_epoch=True)
-        self.log("valid/f1", f1, prog_bar=True, on_epoch=True)
-        return {"labels": labels, "logits": outputs.logits}
+# Compiling the RNN
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 
-    def validation_epoch_end(self, outputs):
-        labels = torch.cat([x["labels"] for x in outputs])
-        logits = torch.cat([x["logits"] for x in outputs])
-        preds = torch.argmax(logits, 1)
-
-        ## There are multiple ways to track the metrics
-        # 1. Confusion matrix plotting using inbuilt W&B method
-        self.logger.experiment.log(
-            {
-                "conf": wandb.plot.confusion_matrix(
-                    probs=logits.numpy(), y_true=labels.numpy()
-                )
-            }
-        )
-
-     
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=self.hparams["lr"])
+# Fitting the RNN to the Training set
+regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
